e4b9a37a3972ffd5c680865b4b7984a9a61aa019
Laven Morrow <lmorrow13@live.com>
2017-10-06 16:10:56 -0600

    intial design and content along with modal

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-placeholder()
+var modal = document.getElementById('myModal');
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+setTimeout(function() {
+    modal.style.display = "block";
+}, 3000);
